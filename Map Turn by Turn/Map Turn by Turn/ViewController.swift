b8aa6a6129faf418ce129989f98f94f7aaa97c2e
Raiyan <raiyan.sharif.1234@gmail.com>
2021-05-21 05:02:13 +0600

    map implemented

--- Map Turn by Turn/Map Turn by Turn/ViewController.swift
+++ Map Turn by Turn/Map Turn by Turn/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var directionLabel: UILabel!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    var currentCoordinate: CLLocationCoordinate2D!
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        
+        locationManager.startUpdatingLocation()
         // Do any additional setup after loading the view.
     }
 
 
 }
+
+extension ViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("location Manager Delegate")
+        manager.stopUpdatingLocation()
+        guard let currentLocation = locations.first else {return}
+        currentCoordinate = currentLocation.coordinate
+        mapView.userTrackingMode = .followWithHeading
+        
+        
+    }
+}
+
+extension ViewController: UISearchBarDelegate{
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("")
+    }
+}
+
+
+extension ViewController: MKMapViewDelegate{
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        return MKOverlayRenderer()
+    }
+}
+
 
